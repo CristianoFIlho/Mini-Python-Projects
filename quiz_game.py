6054a40a9ba1ad828556c37d6ce9ed8e73bbf6af
Cristiano Filho ✅ <54041918+CristianoFIlho@users.noreply.github.com>
2023-11-25 09:12:04 -0300

    Add score tracking and additional question

--- quiz_game.py
+++ quiz_game.py
@@ -11,23 +11,35 @@ if playing.lower() != "yes":
     quit()
 
 print("Okey! Let's play :)")
+score = 0
 
 answer = input("What does CPU stand for?")
 if answer.lower() == "processing unit":
     print("Correct!")
+    score += 1  # Increment the score by 1
 else:
     print("Incorrect!")
 
 answer = input("What does RAM stand for?")
 if answer.lower() == "random access memory":
-    print("Correct!")
+    print("Correct")
+    score += 1  # Increment the score by 1
 else:
     print("Incorrect!")
     
 answer = input("What does PSU stand for?")
 if answer.lower() == "power supply":
-    print("Correct!")
+    print("Correct")
+    score += 1  # Increment the score by 1
 else:
-    print("Incorrect!")    
+    print("Incorrect!")
     
+answer = input("What do you GPU?")
+if answer.lower() == "RTX 4040":
+    print("Correct your score is!" + score)
+    score += 1  # Increment the score by 1
+else:
+    print("Incorrect!")        
+        
+print("You got " + str(score) + " questions correct!")   
    
