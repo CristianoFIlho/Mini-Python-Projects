ee0106a368ee220a5dab0f1cf73050bcd31cfc0f
Cristiano Filho ✅ <54041918+CristianoFIlho@users.noreply.github.com>
2023-11-25 00:24:32 -0300

    Fix quiz_game.py questions and answers

--- quiz_game.py
+++ quiz_game.py
@@ -9,13 +9,20 @@ if playing.lower() != "yes":
 print("Okey! Let's play :)")
 
 answer = input("What does CPU stand for?")
-if answer.lower() == "central processing unit":
+if answer.lower() == "processing unit":
     print("Correct!")
 else:
     print("Incorrect!")
 
-answer = input("What does GPU stand for?")
-if answer.lower() == "graphics processing unit":
+answer = input("What does RAM stand for?")
+if answer.lower() == "random access memory":
     print("Correct!")
 else:
     print("Incorrect!")
+    
+answer = input("What does PSU stand for?")
+if answer.lower() == "power supply":
+    print("Correct!")
+else:
+    print("Incorrect!")    
+    
